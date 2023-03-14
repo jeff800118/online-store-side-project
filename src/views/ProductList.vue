504281fb6f7870017517469a4ddd5c6f4c8a69d0
Jeff <jeff800118@gmail.com>
2023-03-14 17:32:06 +0800

    重新修改goodsDetail的layout

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -8,25 +8,24 @@
             <div><router-link to ="#" :class="{cur:$route.params.type == 5}">{{$route.params.type == 5? "美妝保養":""}}</router-link></div>
         </div>
         <!-- <hr> -->
-        <div class="hotItemBox">
+        <div class="hotItemBox" >
+
             <div class="hotItem" v-for="(item,index) in dataShow" :key="index" v-show="$route.params.type == item.goods_pid">
-                <div class="goodsImg">
-                    <img :src="require(`../assets/${item.goods_img}`)" alt="" >
-                </div>
                 <div class="goodsDetail">
-                    <div>
-                        <ul>
-                            <li><b>商品名稱 :</b> {{item.goods_name}}</li>
-                            <li><b>商品價格 :</b> {{item.goods_price}}</li>
-                            <li><b>商品庫存 :</b> {{item.goods_stock}}</li>
-                            <li><b>商品類型 :</b> {{item.goods_style}}</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <button @click="addToCart(item.goods_num)">加到購物車</button>
-                    </div>
+                    <img :src="require(`../assets/${item.goods_img}`)" alt="" >
+                    <ul>
+                        <li><b>商品名稱 :</b> {{item.goods_name}}</li>
+                        <li><b>商品價格 :</b> {{item.goods_price}}</li>
+                        <li><b>商品庫存 :</b> {{item.goods_stock}}</li>
+                        <li><b>商品類型 :</b> {{item.goods_style}}</li>
+                    </ul>
+                </div>
+                <div class="addToCartBtn">
+                    <button @click="addToCart(item.goods_num)">加到購物車</button>
                 </div>
             </div>
+            <!-- 家道購物車按鈕 -->
+            
         </div>
         <div id="pageList">
             <button @click="prevPage()">上一頁</button>
@@ -48,59 +47,6 @@
                 dataShow:[],
                 goodsList:[],
                 goodsAllList:[],
-                imgList:[
-                    {pid:1,src:require('../assets/3C產品/3C1.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C2.png')},
-                    {pid:1,src:require('../assets/3C產品/3C3.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C4.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C5.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C6.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C7.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C8.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C9.jpg')},
-                    {pid:1,src:require('../assets/3C產品/3C10.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit1.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit2.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit3.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit4.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit5.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit6.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit7.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit8.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit9.jpg')},
-                    {pid:2,src:require('../assets/運動戶外/outfit10.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture1.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture2.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture3.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture4.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture5.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture6.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture7.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture8.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture9.jpg')},
-                    {pid:3,src:require('../assets/家具電器/furniture10.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily1.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily2.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily3.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily4.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily5.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily6.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily7.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily8.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily9.jpg')},
-                    {pid:4,src:require('../assets/生活用品/daily10.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic1.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic2.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic3.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic4.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic5.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic6.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic7.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic8.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic9.jpg')},
-                    {pid:5,src:require('../assets/美妝保養/cosmetic10.jpg')}
-                ],
-                picList:require([])
             }
         },
         methods:{
@@ -148,9 +94,6 @@
                 this.axios.get(url).then((res)=>{
                     console.log(res)
                     this.goodsList = res.data 
-                    // this.goodsAllList.concat(this.goodsList)
-                    // console.log(this.goodsAllList)   
-                    // console.log(this.goodsList)
                     this.$store.commit('addToCart',this.goodsList)
                     console.log(this.$store.state.goods_num)
                     alert('商品已放入購物車')
@@ -167,16 +110,24 @@
 // items項目名稱
 #container{
     margin-top: 100px;
+    display: flex;
+    flex-direction: column;
+    
+}
+
+.category{
+    margin-top:20px;
 }
 
 .hotItemBox{
   display: flex;
   flex-direction:row ;
-  justify-content: space-around;
-//   align-content: space-between;
-  margin-top: 20px;
-  max-width:100%;
   flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  background-color:#f9f2f4;
+  margin-top: 40px;
+  max-width:100%;
   & ul{
     list-style-type: none;
     line-height: 1.5;
@@ -188,37 +139,30 @@
   display: flex;
   flex-direction: column;
   align-items: center;
-  flex-wrap: wrap;
-  flex:0 0 33%;
+  flex:0 0 33% ;
   height:200px;
+  flex-wrap: wrap;
   border: 1px solid #a1a1a1;
   justify-content: center;
-  margin-top: 3px;
-  word-break:break-all;
-  width:25%;
-  & .goodsImg{
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    & img{
-        width: 100%;
-        height: 25%;
-    }
+  margin-top: 3px; 
+  background-color:white ;
+  & img{
+    width: 100%;
+    width: 150px;
+    margin-left: 5px;
   }
-  & .goodsDetail{
-    display: flex;
-    flex-direction: column;
-    justify-items: center;
-    & ul{
-        word-break: break-all;
-    }
-    & button{
+  & button{
         background: orange;
         color:white;
         cursor: pointer;
         width: 100px;
         }
+  & .goodsDetail{
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    word-break:break-all;
+    background-color:white ;
     }
     & b{
         background-color:#f9f2f4;
@@ -238,13 +182,12 @@
 
 #pageList{
     display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     justify-content: center;
     margin: 20px;
     & button ,& a{
-        // background-color: orange;
-        // border: 1px solid black;
         margin: 5px;
-        // color:black;
         border-radius:5px;
         padding: 5px;
         font-weight: 700;
